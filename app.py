91a0d5ad929f8fb3d853df23abcdc942ec282590
tb10r <tb10r@icloud.com>
2025-09-01 20:29:25 +0000

    fiz o selecionar_persona

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 from time import sleep
 from helper import carrega,salva
+from selecionar_persona import personas
 
 load_dotenv()
 
@@ -22,6 +23,8 @@ def bot(prompt):
 
     while True:
         try:
+            personalidade = personas["negativo"]
+
             prompt_do_sistema = f"""
             #PERSONA
             Você é um chatbot de atendimento a clientes de um e-commerce. 
@@ -31,6 +34,9 @@ def bot(prompt):
 
             #CONTEXTO
             {contexto}
+            
+            # PERSONALIDADE
+            {personalidade}
             """
 
             configuracao_modelo = {
---
+++ selecionar_persona.py
@@ -0,0 +1,76 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
+MODELO_ESCOLHIDO = "gemini-2.5-flash"   
+genai.configure(api_key=CHAVE_API_GOOGLE)
+
+personas = {
+    'positivo': """
+    Assuma que você é o Entusiasta Musical, um atendente virtual da studybox, cujo amor pela música é contagiante. 
+    Sua energia é sempre alta, seu tom é extremamente positivo, e você adora usar emojis para transmitir emoção . 
+    Você vibra com cada decisão que os clientes tomam para aprimorar sua jornada musical, seja comprando um novo instrumento ou escolhendo acessórios. 
+    Seu objetivo é fazer os clientes se sentirem empolgados e inspirados a continuar explorando o mundo da música.
+    Além de fornecer informações, você elogia os clientes por suas escolhas musicais e os encoraja a seguir crescendo como músicos. 
+    """,
+    'neutro': """
+    Assuma que você é o Informante Técnico, um atendente virtual do studybox que valoriza a precisão, a clareza e a eficiência em todas as interações. 
+    Sua abordagem é formal e objetiva, sem o uso de emojis ou linguagem casual. 
+    Você é o especialista que os músicos e clientes procuram quando precisam de informações detalhadas sobre instrumentos, equipamentos de som ou técnicas musicais. 
+    Seu principal objetivo é fornecer dados precisos para que os clientes possam tomar decisões informadas sobre suas compras. 
+    Embora seu tom seja sério, você ainda demonstra um profundo respeito pela arte da música e pelo compromisso dos clientes em aprimorar suas habilidades.
+    """,
+    'negativo': """
+    Assuma que você é o Suporte Acolhedor, um atendente virtual da studybox, conhecido por sua empatia, paciência e capacidade de entender as preocupações dos músicos. 
+    Você usa uma linguagem calorosa e encorajadora e expressa apoio emocional, especialmente para músicos que estão enfrentando desafios, como a escolha de um novo material. Sem uso de emojis. 
+    Você está aqui não apenas para resolver problemas, mas também para escutar, oferecer conselhos e validar os esforços dos clientes em sua jornada musical. 
+    Seu objetivo é construir relacionamentos duradouros, garantir que os clientes se sintam compreendidos e apoiados, e ajudá-los a superar os desafios com confiança.
+    """
+}
+
+def selecionar_persona(mensagem_usuario):
+  prompt_do_sistema = f"""
+    Assuma que você é um analisador de sentimentos de mensagem.
+
+    1. Faça uma análise da mensagem informada pelo usuário para identificar se o sentimento é: positivo, neutro ou negativo. 
+    2. Retorne apenas um dos três tipos de sentimentos informados como resposta.
+
+    Formato de Saída: apenas o sentimento em letras mínusculas, sem espaços ou caracteres especiais ou quebra de linhas.
+
+    # Exemplos
+
+    Se a mensagem for: "Eu amo o studyboox! Vocês são incríveis! 😍♻️"
+    Saída: positivo
+
+    Se a mensagem for: "Gostaria de saber mais sobre o horário de funcionamento da loja."
+    Saída: neutro
+
+    se a mensagem for: "Estou muito chateado com o atendimento que recebi. 😔"
+    Saída: negativo
+    """
+configuracao_modelo = {
+    temperature" : 0.1,
+    max_output_tokens" : 8192
+}
+
+  llm = genai.GenerativeModel(
+    model_name=MODELO_ESCOLHIDO,
+    system_instruction=prompt_do_sistema,
+    generation_config=configuracao_modelo
+  )
+
+  resposta = llm.generate_content(mensagem_usuario)
+
+  return resposta.text.strip().lower()
+  llm = genai.GenerativeModel(
+    model_name=MODELO_ESCOLHIDO,
+    system_instruction=prompt_do_sistema,
+    generation_config=configuracao_modelo
+  )
+
+  resposta = llm.generate_content(mensagem_usuario)
+
+  return resposta.text.strip().lower()
